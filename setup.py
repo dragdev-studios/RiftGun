904f1a66aa1261235cb8623d03623ae7491d9a56
dragdev <samueljohnnicholson@gmail.com>
2020-06-22 18:55:49 +0100

    0.5
    Signed-off-by: dragdev <samueljohnnicholson@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='riftgun',
-    version='0.4',
+    version='0.5',
     packages=['riftgun'],
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
