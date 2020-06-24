a7096af5c7f0f434e2230eb3c92839de0e8c7d8c
dragdev <samueljohnnicholson@gmail.com>
2020-06-24 16:55:41 +0100

    Add GuildConverter and make it more uniform
    Signed-off-by: dragdev <samueljohnnicholson@gmail.com>

--- riftgun/converters.py
+++ riftgun/converters.py
@@ -1,10 +1,21 @@
 import discord
-import typing
 from discord.ext import commands
 
 
-class GlobalTextChannel(commands.Converter):
+class GlobalConverter(commands.Converter):
+    """The ABC class for global converters"""
+
+    async def convert(self, ctx: commands.Context, argument: str):
+        raise NotImplementedError
+
+    @staticmethod
+    def convertSync(ctx: commands.Context, argument: str):
+        raise NotImplementedError
+
+
+class GlobalTextChannel(GlobalConverter):
     """A converter that attempts to find the closest match to the provided channel."""
+
     def __init__(self, *, sort_by_last_message: bool = True):
         self.sblm = sort_by_last_message
 
@@ -31,8 +42,10 @@ class GlobalTextChannel(commands.Converter):
 
         channel = discord.utils.find(match, sorted(list(ctx.bot.get_all_channels()), key=lambda x: x.id))
 
-        if channel: return channel
-        else: raise commands.BadArgument(f"Unable to convert \"{argument}\" to TextChannel, globally or locally.")
+        if channel:
+            return channel
+        else:
+            raise commands.BadArgument(f"Unable to convert \"{argument}\" to TextChannel, globally or locally.")
 
     @staticmethod
     def convertSync(ctx, argument: str) -> discord.TextChannel:
@@ -53,5 +66,29 @@ class GlobalTextChannel(commands.Converter):
 
         channel = discord.utils.find(match, sorted(list(ctx.bot.get_all_channels()), key=lambda x: x.id))
 
-        if channel: return channel
-        else: raise commands.BadArgument(f"Unable to convert \"{argument}\" to TextChannel, globally or locally.")
+        if channel:
+            return channel
+        else:
+            raise commands.BadArgument(f"Unable to convert \"{argument}\" to TextChannel, globally or locally.")
+
+
+class GuildConverter(GlobalConverter):
+    """Converts a provided guild name/id into a discord.Guild"""
+
+    @staticmethod
+    def convertSync(ctx: commands.Context, argument: str):
+        for guild in ctx.bot.guilds:
+            if str(guild.id) == argument:
+                return guild
+            elif guild.name.lower() == argument.lower():
+                return guild
+            elif guild.name.lower() in argument.lower():
+                return guild
+            elif argument.lower() in guild.name.lower():
+                return guild
+            else:
+                continue
+        raise commands.BadArgument(f"Unable to convert \"{argument}\" to guild.")
+
+    async def convert(self, ctx: commands.Context, argument: str):
+        return self.convertSync(ctx, argument)
